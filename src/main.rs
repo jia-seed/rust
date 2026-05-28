8f8487c38e2adc97b9e4ad6b58c44268d4b422bc
Audrey Chen <Audgeviolin07@gmail.com>
2026-05-28 01:40:57 -0700

    semicolon

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use tokio::net::TcpListener;
 async fn main() {
     let app = Router::new()
         .route("/", get(|| async { "meow meow rust rust xP" }))
-        .route("/favicon.ico", get(favicon))
+        .route("/favicon.ico", get(favicon));
 
     let port = std::env::var("PORT").unwrap_or_else(|_| "3000".into());
     let addr = format!("0.0.0.0:{port}");
